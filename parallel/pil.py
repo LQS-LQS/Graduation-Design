0c8d9a008ff710b36b473d5e7076c55de1fd32c9
lqs-global <fuck-global@qq.com>
2023-05-01 20:52:42 +0800

    feat:删除

--- parallel/pil.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-# 读取JPEG图像
-im = Image.open('D:\\MyCode-Git\\Graduation-Design\\source_2.jpg')
-
-# 保存图像为JPEG格式
-im.save('D:\\MyCode-Git\\Graduation-Design\\output2.jpg', quality=100)
